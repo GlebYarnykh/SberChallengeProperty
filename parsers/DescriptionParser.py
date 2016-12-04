145b3cfc1bc67e204ca03f63211fa0c1bba975a6
Mikhail-Borisov <npoka3hbuk@gmail.com>
2016-12-04 16:04:40 +0300

    Added spellchecking and lemmatization

--- parsers/DescriptionParser.py
+++ parsers/DescriptionParser.py
@@ -3,7 +3,42 @@ import numpy as np
 import pandas as pd
 import nltk
 import pymorphy2
+import json
+import requests
+
 morph = pymorphy2.MorphAnalyzer()
+
+
+def _request_spelling(text):
+    params = {'text': text,
+              'lang': 'ru',
+              'options': 783,
+              }
+    response = requests.get('http://speller.yandex.net/services/spellservice.json/checkText', params=params)
+    return response
+
+
+def _correct_spelling(text):
+    response = _request_spelling(text)
+    rj = response.json()
+    num_errors = len(rj)
+    if response.status_code == 200:
+        if num_errors > 0:
+            for i in xrange(num_errors):
+                error = rj[i]
+                if error[u'code'] == 1:
+                    suggestions = [v for v in error[u's']]
+                    suggestion = suggestions[0]
+                    start = error[u'pos']
+                    end = start + error[u'len']
+                    text = text[:start] + text[start:end].replace(error[u'word'], suggestion) + text[end:]
+                if error[u'code'] == 2:
+                    start = error[u'pos']
+                    end = start + error[u'len']
+                    text = text[:(start-1)] + text[end:]
+                if error[u'code'] == 4:
+                    pass
+    return text
 
 
 def _tokenize(description):
@@ -15,9 +50,11 @@ def _tokenize(description):
 def _lemmatize(word):
     global morph
     lemma = morph.parse(word)
-    print(lemma)
-    normalform = lemma[0].normal_form
-    return normalform
+    normalform_0 = lemma[0].normal_form
+    return normalform_0
+
+
+_lemmatize_list = np.vectorize(_lemmatize)
 
 
 def parse_description(dataframe, column_name='_DESC_'):
@@ -25,7 +62,15 @@ def parse_description(dataframe, column_name='_DESC_'):
 
 if __name__ == '__main__':
     data = pd.read_csv('../champ1_train.csv')
-    test_case = data['_DESC_'][1]
-    tokens = _tokenize(test_case)
-    lemmas = _lemmatize(u'парковочных')
-    print(lemmas)
+    N = 3
+    test_cases = []
+    tokens = []
+    lemmas = []
+    string = []
+    for i in xrange(N):
+        test_cases.append(data['_DESC_'][i])
+        test_cases[i] = _correct_spelling(test_cases[i])
+        tokens.append(_tokenize(test_cases[i]))
+        lemmas.append(_lemmatize_list(tokens[i]))
+        string.append(' '.join(lemmas[i]))
+        print(string[i])
